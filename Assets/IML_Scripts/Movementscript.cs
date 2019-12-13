c375af3d60e77dafa5993bc028ea6e39fa1b55ed
Maliaismaelunity <luzu7barbate@gmail.com>
2019-12-13 09:44:56 +0100

    IML_13/12/2019
    Final Commit

---
+++ Assets/IML_Scripts/Movementscript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Movementscript : MonoBehaviour
+{
+
+    public GameObject destino;
+    private UnityEngine.AI.NavMeshAgent agenteDeNavegacion;
+
+    // Use this for initialization
+    void Start()
+    {
+        agenteDeNavegacion = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agenteDeNavegacion.SetDestination(destino.transform.position);
+    }
+
+}
